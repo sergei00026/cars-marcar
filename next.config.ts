285486ce5352ab1e4e87ee3ae46a4c83baf53dc4
sergei00026 <sergei57fx@mail.ru>
2025-06-18 15:43:29 +0300

    add redirect

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['ru-msk-dr3-1.store.cloud.mts.ru'], // ваш домен с изображениями
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/cars',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
