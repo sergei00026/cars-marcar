8c8d997467f845d9792345dadeb09b08977c19ce
sergei00026 <sergei57fx@mail.ru>
2025-06-18 15:03:18 +0300

    for versel 4

--- src/app/cars/page.tsx
+++ src/app/cars/page.tsx
@@ -3,9 +3,13 @@ import type { Car } from '@/app/types/type';
 import { CarCard } from '@/app/components/carCard/CarCard';
 import { Pagination } from '@/app/components/pagination/Pagination';
 
+interface PageProps {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}
+
 export const dynamic = 'force-dynamic';
 
-export default async function CarsPage({ searchParams }: any) {
+export default async function CarsPage({ searchParams }: PageProps) {
   const pageRaw = searchParams?.page;
   const sortRaw = searchParams?.sort;
 
